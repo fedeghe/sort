241cd4174e491e284cdd7f85bbee71b2ce72d05a
fedeghe <fedeghe@gmail.com>
2020-02-27 08:37:17 +0100

    quickFunc

--- quick.spec.js
+++ quick.spec.js
@@ -1,33 +1,38 @@
-const quick = require('./quick')
+const quick = require('./quick'),
+    quickFunc = require('./quickfunc');
 
 describe('sort ', () => {
-    const len = 1e5;
+    const len = 1e1;
     const base = Array.from({length: len}, i => i)
-    const MID = base.map(() => parseInt(Math.random()*len), 10),
-        WORST = base.map((el, i) => 1),
-        BEST = base.map((el, i) => i);
+    const SET = base.map(() => parseInt(Math.random()*len), 10);
+    const SETfunc = base.map(() => ({
+        num: parseInt(Math.random()*len, 10)
+    }));
 
-        // console.log(MID)
     it('quick', () => {
-        const mid0 = performance.now(),
-            mid = quick([...MID]),
-            mid1 = performance.now(),
+        const start = performance.now(),
+            ordererSET = quick([...SET]),
+            end = performance.now(),
+            startDef = performance.now(),
+            ordererDEF = [...SET].sort((a, b) => a > b ? 1 : -1),
+            endDef = performance.now();
 
-            worst0 = performance.now(),
-            worst = quick([...WORST]),
-            worst1 = performance.now(),
-            
-            best0 = performance.now(),
-            best = quick([...BEST]),
-            best1 = performance.now();
+        expect(ordererSET).toEqual(ordererDEF);
+        console.log('time: ', (end - start).toFixed(1)+'ms')
+        console.log('def: ', (endDef - startDef).toFixed(1)+'ms')
+        
+    });
+    it('quickFunc', () => {
+        const start = performance.now(),
+            ordererSET = quickFunc([...SETfunc], (a, b) => a.num < b.num, (a, b) => a.num > b.num),
+            end = performance.now(),
+            startDef = performance.now(),
+            ordererDEF = [...SETfunc].sort((a, b) => a.num > b.num ? 1 : -1),
+            endDef = performance.now();
 
-        expect(true).toEqual(true);
-        expect(mid).toEqual([...MID].sort((a, b) => a > b ? 1 : -1));
-        expect(worst).toEqual([...WORST].sort((a, b) => a > b ? 1 : -1));
-        expect(best).toEqual([...BEST].sort((a, b) => a > b ? 1 : -1));
-        console.log('mid: ', (mid1-mid0).toFixed(1)+'ms')
-        console.log('worst: ', (worst1-worst0).toFixed(1)+'ms')
-        console.log('best: ', (best1-best0).toFixed(1)+'ms')
+        expect(ordererSET).toEqual(ordererDEF);
+        console.log('time: ', (end - start).toFixed(1)+'ms')
+        console.log('def: ', (endDef - startDef).toFixed(1)+'ms')
         
     });
 });
--- quickfunc.js
+++ quickfunc.js
@@ -4,15 +4,15 @@ function swap(items, leftIndex, rightIndex){
     items[leftIndex] = items[rightIndex];
     items[rightIndex] = temp;
 }
-function partition(items, left, right) {
+function partition(items, fn1, fn2, left, right) {
     // var pivot   = items[Math.floor((right + left) / 2)],
     var pivot = items[(right+left)  >>> 1], //middle element
         i = left, //left pointer
         j = right; //right pointer
     while (i <= j) {
         
-        while (items[i] < pivot)i++;
-        while (items[j] > pivot) j--;
+        while (fn1(items[i], pivot))i++;
+        while (fn2(items[j], pivot)) j--;
         if (i <= j) {
             swap(items, i, j); //sawpping two elements
             i++;
@@ -22,18 +22,18 @@ function partition(items, left, right) {
     return i;
 }
 
-function quickSort(items, left, right) {
+function quickSort(items, fn1, fn2, left, right) {
     var index;
     if (items.length > 1) {
-        index = partition(items, left, right); //index returned from partition
+        index = partition(items, fn1, fn2, left, right); //index returned from partition
         if (left < index - 1) { //more elements on the left side of the pivot
-            quickSort(items, left, index - 1);
+            quickSort(items, fn1, fn2, left, index - 1);
         }
         if (index < right) { //more elements on the right side of the pivot
-            quickSort(items, index, right);
+            quickSort(items, fn1, fn2, index, right);
         }
     }
     return items;
 }
 
-module.exports = a => quickSort(a, 0, a.length - 1);
+module.exports = (a, fn1, fn2) => quickSort(a, fn1, fn2, 0, a.length - 1);
