58db9a9a6505edc840f96370f933c824c3f6566d
fedeghe <fedeghe@gmail.com>
2020-02-29 00:19:47 +0100

    some minor refactor

--- bitonic.js
+++ bitonic.js
@@ -1,25 +1,20 @@
-function kernel(x, p, q) {
-    const y = new Array(x.length),
-        d = 1 << (p - q);
+const kernel = (x, p, q) => {
+    const d = 1 << (p - q);
   
     for (let i = 0; i < x.length; i++) {
         const up = ((i >> p) & 2) === 0;
-
-        if ((i & d) == 0 && (x[i] > x[i + d]) === up) {
-            y[i] =  x[i + d];
-        } else if ((i & d) == d && (x[i - d] > x[i]) === up) {
-            y[i] = x[i - d];
-        } else {
-            y[i] = x[i];
+        if ((i & d) == 0 && (x[i] > x[i | d]) === up) {
+            const tmp = x[i];
+            x[i] = x[i | d];
+            x[i | d] = tmp;
         }
     }
-    return y;
 }
   
-function bitonicsort(x, n) {
+const bitonicsort = (x, n) => {
     for (let i = 0; i < n; i++) {
         for(let j = 0; j <=i; j++) {
-            x = kernel(x, i, j);
+            kernel(x, i, j);
         }
     }
     return x;
