b4f9bd748613e7915504162b542fcb2a3953ff9b
fedeghe <fedeghe@gmail.com>
2020-02-29 00:20:12 +0100

    some minor refactor

--- insertion.js
+++ insertion.js
@@ -1,6 +1,7 @@
 let insertion = (arr, fn) => {
-    let length = arr.length;
-    for (let i = 1; i < length; i++) {
+    let length = arr.length,
+        i = 1;
+    for (; i < length; i++) {
         let key = arr[i],
             j = i - 1;
         while (j >= 0 && fn(arr[j], key)) {
