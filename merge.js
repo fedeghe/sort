2c3ecc2b483b7242a524d5699d339c571e04a886
fedeghe <fedeghe@gmail.com>
2020-02-29 00:20:19 +0100

    some minor refactor

--- merge.js
+++ merge.js
@@ -1,33 +1,32 @@
-function merge (left, right, fn) {
+const merge = (left, right, fn) => {
     let res = [],
         leftI = 0, rightI = 0,
         leftL = left.length, rightL = right.length;
 
     while (leftI < leftL && rightI < rightL) {
-      if (fn(left[leftI], right[rightI])) {
-        res.push(left[leftI]);
-        leftI++;
-      } else {
-        res.push(right[rightI]);
-        rightI++;
-      }
+        if (fn(left[leftI], right[rightI])) {
+            res.push(left[leftI]);
+            leftI++;
+        } else {
+            res.push(right[rightI]);
+            rightI++;
+        }
     }
-  
     return res
         .concat(left.slice(leftI))
         .concat(right.slice(rightI));
 }
-function mergeSort (arr, fn) {
+const mergeSort = (arr, fn) => {
     if (arr.length <= 1) {
-      return arr;
+        return arr;
     }
     const middle = arr.length >>> 1,
         left = arr.slice(0, middle),
         right = arr.slice(middle);
     return merge(
-      mergeSort(left, fn),
-      mergeSort(right, fn),
-      fn
+        mergeSort(left, fn),
+        mergeSort(right, fn),
+        fn
     );
 }
 
