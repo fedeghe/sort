618d53d01d5efb42f0cd415b3179fac826dbff2d
fedeghe <fedeghe@gmail.com>
2020-02-29 00:20:29 +0100

    some minor refactor

--- radix.js
+++ radix.js
@@ -1,20 +1,14 @@
-function radixSort(arr) {
-    // Find the max number and multiply it by 10 to get a number
-    // with no. of digits of max + 1
+const radixSort = arr => {
     const maxNum = Math.max(...arr) * 10;
-    let divisor = 10;
-    while (divisor < maxNum) {
-       // Create bucket arrays for each of 0-9
-       let buckets = [...Array(10)].map(() => []);
-       // For each number, get the current significant digit and put it in the respective bucket
-       for (let num of arr) {
-          buckets[Math.floor((num % divisor) / (divisor / 10))].push(num);
-       }
-       // Reconstruct the array by concatinating all sub arrays
-       arr = [].concat.apply([], buckets);
-       // Move to the next significant digit
-       divisor *= 10;
+    let div = 10;
+    while (div < maxNum) {
+        let buckets = [...Array(10)].map(() => []);
+        for (let num of arr) {
+            buckets[Math.floor((num % div) / (div / 10))].push(num);
+        }
+        arr = [].concat.apply([], buckets);
+        div *= 10;
     }
     return arr;
- }
+}
 module.exports = a => radixSort(a)
