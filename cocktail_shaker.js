c584f5a68df916d498bd31bfd5a227af70ba45ee
fedeghe <fedeghe@gmail.com>
2020-02-28 23:42:23 +0100

    cocktail shaker added

--- cocktail_shaker.js
+++ cocktail_shaker.js
@@ -1,5 +1,26 @@
-/**
- * PLEEEEASE do not do it!!!
- */
+const swap = require('./utils.js').swap;
+const shaker = (arr, fn) => {
+    let max = arr.length - 1;
+    let min = 0;
+    while(min < max){
+        let biggest = min;
+        let smallest = max;
+        for (var i = min; i <= max; i++)
+            if (fn(arr[i], arr[biggest]))
+                biggest = i;
+        if(max != biggest){ //swap the items
+            swap(arr, biggest, max)
+        }
+        max--;
+        for (var j = max; j >= min; j--)
+            if(fn(arr[smallest], arr[j]))
+                smallest = j;
+        if(min != smallest) { //swap the items
+            swap(arr, smallest, min);
+        }
+        min++;
+    }  
+    return arr;
+}
 
-module.exports = a => cocktail
+module.exports = (a, fn) => shaker(a, fn)
