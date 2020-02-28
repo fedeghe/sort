28eb5c46c8e5946649a58dfd03910a209fd1c192
fedeghe <fedeghe@gmail.com>
2020-02-28 21:43:07 +0100

    faster counting

---
+++ counting2.js
@@ -0,0 +1,19 @@
+const counting = (arr) => {
+    let i = 0,
+        j = 0,
+        len = arr.length,
+        count = [];
+
+    for (i = 0; i < len; i++) {
+        count[arr[i]] = typeof count[arr[i]] !== 'undefined'
+            ? count[arr[i]] + 1
+            : 1;
+    }
+
+    return count.reduce((acc, el, i) => {
+        while(el--) acc.push(i)
+        return acc
+    }, [])
+};
+
+module.exports = a => counting(a)
