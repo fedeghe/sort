642075c4534d125de42a3bdb14f59e8a619a76c7
fedeghe <fedeghe@gmail.com>
2020-03-06 22:19:46 +0100

    string generate

--- benchmark/randomArr.js
+++ benchmark/randomArr.js
@@ -6,10 +6,22 @@ const len = 1e2,
     SET2pow = base2pow.map(() => parseInt(Math.random() * len), 10),
     SETobj = base.map(() => ({
         num: parseInt(Math.random() * len, 10)
-    }));
+    })),
+    charsets = [[65,90], [97,122], [48,57]],
+    charset = charsets.reduce((acc, el) => {
+        let a = [];
+        for(let h = el[0]; h <=el[1]; h++) a.push(h)
+        return acc.concat(a)
+    }, []),
+    getRndString = len => 
+        Array.from({length: len}, () => String.fromCharCode(
+            charset[~~(Math.random() * charset.length)]
+        )).join(''),
+    SETSTRING = base.map(i => getRndString(8));
 module.exports = {
     SET,
     SETobj,
     SET2pow,
+    SETSTRING,
     len
 };
